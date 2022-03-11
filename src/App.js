ca2865863d5e699b714077479a7f2a54bbb29683
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-11 22:52:26 +0330

    updated app.css and js file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import Weather from "./Weather";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World React</h1>
-        <Weather city="Tehran" />
-      </header>
-    </div>
-  );
+  return <Weather city="Tehran" />;
 }
 
 export default App;
