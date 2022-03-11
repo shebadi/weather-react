1b0f290d280ac62e16fdb5698e53adda0538698e
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-11 22:01:36 +0330

    Added react-loader-spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -8,5 +9,10 @@ export default function Weather(props) {
   let apiKey = "7a088f73941e6a828a29489663c7f3f7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h1>Hello Weather </h1>;
+  return (
+    <h1>
+      Hello Weather
+      <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />
+    </h1>
+  );
 }
