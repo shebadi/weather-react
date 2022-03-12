032c0aebc7b9e5d98ee70f31cc0c1cc02f076ed0
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-12 18:17:14 +0330

    change footer in js file

--- src/Weather.js
+++ src/Weather.js
@@ -3,5 +3,27 @@ import WeatherSearch from "./WeatherSearch";
 import "./Weather.css";
 
 export default function Weather(props) {
-  return <WeatherSearch />;
+  return (
+    <div>
+      <WeatherSearch />
+      <footer>
+        This project was coded by Shabnam and is{" "}
+        <a
+          href="https://github.com/shebadi/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://laughing-tesla-b02d66.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
